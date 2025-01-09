bf0513dc7eb1670512f00cb3d6fbb36b48e27ee4
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-01-09 21:01:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,116 @@
+// String o'zgaruvchilar (const)
+const ismingizniYozing = 'Ibrohimjon'
+const familiyaYozing =' Athamjonov'
+const mazilYzonig = '58'
+const shaharYozing =' Uzbekistan'
+const yoqtirganRang = 'Yashil'
+const yoqtirganMashina = 'Malibu'
+const yoqtirganOvqat =' pizza'
+const yoqtirganFan = 'It'
+const televizor = 'televizor'
+const notebook = 'notebook'
+
+console.log(typeof ismingizniYozing);
+console.log(typeof familiyaYozing);
+console.log(typeof mazilYzonig);
+console.log(typeof shaharYozing);
+console.log(typeof yoqtirganRang);
+console.log(typeof yoqtirganMashina);
+console.log(typeof yoqtirganOvqat);
+console.log(typeof yoqtirganFan);
+console.log(typeof notebook);
+console.log(typeof televizor);
+
+// Number o'zgaruvchilar (let)
+
+let a = 1
+let b = 2
+let d = 3
+let c = 4
+let v = 5
+let f = 6
+let j = 7
+let p = 8
+let o = 9
+let i = 10
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof d);
+console.log(typeof c);
+console.log(typeof v);
+console.log(typeof f);
+console.log(typeof j);
+console.log(typeof p);
+console.log(typeof o);
+console.log(typeof i);
+
+// Boolean o'zgaruvchilar (const)
+
+const rost = true
+const yolgon = false
+const mashina = true
+const notebuk = false
+const Televizor = true
+const shashka = false
+const shaxmat = true
+const wifi = false
+const telefon = true
+const mishka = false
+
+console.log(typeof rost);
+console.log(typeof yolgon);
+console.log(typeof mashina);
+console.log(typeof notebuk);
+console.log(typeof Televizor);
+console.log(typeof shashka);
+console.log(typeof shaxmat);
+console.log(typeof wifi);
+console.log(typeof telefon);
+console.log(typeof mishka);
+
+// Symbol o'zgaruvchilar (let)
+
+let ibrohimjon = Symbol("ibrohimjon")
+let azimjon = Symbol("azimjon")
+let ismoiljon = Symbol("ismoiljon")
+let azam = Symbol("azam")
+let javohir = Symbol("javohir")
+let abdulaziz = Symbol("abdulaziz")
+let sardor = Symbol("sardor")
+let laziz = Symbol("laziz")
+let omon = Symbol("omon")
+let oybek = Symbol("oybek")
+
+console.log(typeof ibrohimjon);
+console.log(typeof azimjon);
+console.log(typeof ismoiljon);
+console.log(typeof azam);
+console.log(typeof javohir);
+console.log(typeof abdulaziz);
+console.log(typeof sardor);
+console.log(typeof laziz);
+console.log(typeof omon);
+console.log(typeof oybek);
+
+// BigInt o'zgaruvchilar (const)
+
+const bigh = BigInt(47895612314564865488)
+const bigj = BigInt(146897324862134)
+const bigd = BigInt(11111111111111111)
+const bigs = BigInt(55555555555555)
+const bigk = BigInt(8888888888888888)
+const bigl = BigInt(77777777777777)
+const bigi = BigInt(2222222222222222222)
+const bigr = BigInt(999999999999999)
+const bigy = BigInt(1111111111111)
+
+console.log( typeof bigh);
+console.log(typeof bigj);
+console.log(typeof bigd);
+console.log(typeof bigs);
+console.log(typeof bigk);
+console.log(typeof bigl);
+console.log(typeof bigi);
+console.log(typeof bigr);
+console.log(typeof bigy);
